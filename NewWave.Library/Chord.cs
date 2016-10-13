196bb73435dfad9b6feb3566c66e82ec6976e757
Andrew Arnold <andrew.s.arnold@gmail.com>
2016-10-12 21:22:13 -0400

    Library project - starting with some chords

---
+++ NewWave.Library/Chord.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using NewWave.Midi;
+
+namespace NewWave.Library
+{
+	public class Chord
+	{
+		private readonly Pitch _basePitch;
+		private readonly ChordQuality _quality;
+		private readonly ChordAdded _added;
+
+		public Chord(Pitch basePitch, ChordQuality quality, ChordAdded added)
+		{
+			_basePitch = basePitch;
+			_quality = quality;
+			_added = added;
+		}
+
+		public IEnumerable<Pitch> Pitches()
+		{
+			var pitches = new List<Pitch> { _basePitch };
+
+			// Second note
+			if (_quality == ChordQuality.Minor || _quality == ChordQuality.Diminished)
+			{
+				pitches.Add(_basePitch + 3);
+			}
+			else
+			{
+				pitches.Add(_basePitch + 4);
+			}
+
+			// Third note
+			pitches.Add(_basePitch + 7);
+
+			// Fourth note
+			if (_added != ChordAdded.None)
+			{
+				switch (_added)
+				{
+					case ChordAdded.Six:
+						pitches.Add(_basePitch + 9);
+						break;
+					case ChordAdded.Seven:
+						pitches.Add(_basePitch + (_quality == ChordQuality.Major ? 11 : 10));
+						break;
+					case ChordAdded.Nine:
+					case ChordAdded.Eleven:
+						// Not implemented yet
+						break;
+				}
+			}
+
+			return pitches;
+		}
+
+		public override string ToString()
+		{
+			var quality = string.Empty;
+			switch (_quality)
+			{
+				case ChordQuality.Major:
+					quality = "maj";
+					break;
+				case ChordQuality.Minor:
+					quality = "min";
+					break;
+				case ChordQuality.Augmented:
+					quality = "aug";
+					break;
+				case ChordQuality.Diminished:
+					quality = "dim";
+					break;
+			}
+
+			var added = string.Empty;
+			switch (_added)
+			{
+				case ChordAdded.Six:
+					quality = "6";
+					break;
+				case ChordAdded.Seven:
+					quality = "7";
+					break;
+				case ChordAdded.Nine:
+					quality = "9";
+					break;
+				case ChordAdded.Eleven:
+					quality = "11";
+					break;
+			}
+
+			return string.Format("{0}{1}{2}", _basePitch.NoteName(), quality, added);
+		}
+	}
+
+	public enum ChordQuality
+	{
+		NotSpecified,
+		Major,
+		Minor,
+		Augmented,
+		Diminished
+	}
+
+	public enum ChordAdded
+	{
+		None,
+		Six,
+		Seven,
+		Nine,
+		Eleven
+	}
+}
---
+++ NewWave.Test/LibraryTests/ChordTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NewWave.Library;
+using NewWave.Midi;
+
+namespace NewWave.Test.LibraryTests
+{
+	[TestClass]
+	public class ChordTests
+	{
+		[TestMethod]
+		public void CChordTest()
+		{
+			var chord = new Chord(Pitch.C3, ChordQuality.NotSpecified, ChordAdded.None);
+			var pitches = chord.Pitches().ToList();
+			Assert.AreEqual(3, pitches.Count);
+
+			Assert.AreEqual(Pitch.C3, pitches[0]);
+			Assert.AreEqual(Pitch.E3, pitches[1]);
+			Assert.AreEqual(Pitch.G3, pitches[2]);
+		}
+
+		[TestMethod]
+		public void CminChordTest()
+		{
+			var chord = new Chord(Pitch.C3, ChordQuality.Minor, ChordAdded.None);
+			var pitches = chord.Pitches().ToList();
+			Assert.AreEqual(3, pitches.Count);
+
+			Assert.AreEqual(Pitch.C3, pitches[0]);
+			Assert.AreEqual(Pitch.DSharp3, pitches[1]);
+			Assert.AreEqual(Pitch.G3, pitches[2]);
+		}
+
+		[TestMethod]
+		public void Cmin7ChordTest()
+		{
+			var chord = new Chord(Pitch.C3, ChordQuality.Minor, ChordAdded.Seven);
+			var pitches = chord.Pitches().ToList();
+			Assert.AreEqual(4, pitches.Count);
+
+			Assert.AreEqual(Pitch.C3, pitches[0]);
+			Assert.AreEqual(Pitch.DSharp3, pitches[1]);
+			Assert.AreEqual(Pitch.G3, pitches[2]);
+			Assert.AreEqual(Pitch.ASharp3, pitches[3]);
+		}
+	}
+}
