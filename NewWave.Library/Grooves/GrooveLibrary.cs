4869ee8681b9ba054b2babc113e912f1000ff156
Andrew S. Arnold <andrew.s.arnold@gmail.com>
2016-11-03 16:07:31 -0400

    Fixing test song

--- NewWave.Library/Grooves/GrooveLibrary.cs
+++ NewWave.Library/Grooves/GrooveLibrary.cs
@@ -13,6 +13,7 @@ namespace NewWave.Library.Grooves
 			new Groove("funky", TimeSignature.CommonTime, 4, 2, new List<double> { 0, 0.5, 1.75, 2.25, 2.75 }, new List<double> { 1, 3 }),
 			new Groove("backward rock", TimeSignature.CommonTime, 4, 2, new List<double> { 0.5, 1.5, 2.5, 3.5 }, new List<double> { 0, 1, 2, 3 }),
 			new Groove("fake triplet", TimeSignature.CommonTime, 4, 2, new List<double> { 0, 0.75, 1.5 }, new List<double> { 2, 3.5, 3.75 }),
+			new Groove("blastbeat", TimeSignature.CommonTime, 4, 2, new List<double> { 0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5 }, new List<double> { 0.25, 0.75, 1.25, 1.75, 2.25, 2.75, 3.25, 3.75 }),
 		};
 
 		public static Groove GetGroove()
