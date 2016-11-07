3fd665ae4b3ba8b5d6e8e2a7fc98dc87cb0687fc
Andrew S. Arnold <andrew.s.arnold@gmail.com>
2016-11-07 16:51:15 -0500

    Further refactoring

--- NewWave.Generator/SongSection.cs
+++ NewWave.Generator/SongSection.cs
@@ -17,6 +17,10 @@ namespace NewWave.Generator
 		private readonly InstrumentTrack _bass;
 		private readonly PercussionTrack _drums;
 
+		private readonly int _measures = 8;
+		internal List<Chord> Chords;
+		private Groove _groove;
+
 		internal SongSection(TimeSignature time, InstrumentTrack guitarR, InstrumentTrack guitarL, InstrumentTrack bass, PercussionTrack drums)
 		{
 			_time = time;
@@ -24,32 +28,21 @@ namespace NewWave.Generator
 			_guitarL = guitarL;
 			_bass = bass;
 			_drums = drums;
+
+			GetChordProgression();
+			GetGroove();
 		}
 
 		internal int Render()
 		{
-			const int measures = 8;
 			var timeKeepers = new List<Percussion> { Percussion.ClosedHiHat, Percussion.OpenHiHat, Percussion.RideCymbal1 };
 			var timeKeeper = timeKeepers[Randomizer.Next(timeKeepers.Count)];
 
-			List<Chord> chords;
-			do
+			for (var measure = 0; measure < _measures; measure++)
 			{
-				chords = ChordProgressionGenerator.ChordProgression(Pitch.G0, MinorOrDiminshedFilter);
-			} while (chords.Count <= 2 && chords.Count(c => c.Quality == ChordQuality.Minor) < 2);
-
-			if (chords.Count == 3)
-			{
-				chords = AugmentThreeChordProgression(chords);
-			}
-
-			var groove = GrooveLibrary.AllGrooves[Randomizer.Next(GrooveLibrary.AllGrooves.Count)];
-
-			for (var measure = 0; measure < measures; measure++)
-			{
-				var chordIndex = measure % chords.Count;
-				var pitches = chords[chordIndex].Pitches();
-				var grooveNotes = groove.Notes(timeKeeper, measure == 0, _time).ToList();
+				var chordIndex = measure % Chords.Count;
+				var pitches = Chords[chordIndex].Pitches();
+				var grooveNotes = _groove.Notes(timeKeeper, measure == 0, _time).ToList();
 				var kicks = grooveNotes.Where(gn => gn.Percussion == timeKeeper || gn.Start == 0).ToList();
 
 				_guitarR.Notes.Add(kicks.SelectMany((gn, i) => pitches.Take(2).Select(p => new Note(gn.Start, GetLength(gn, i, kicks), p, Velocity.F))).ToList());
@@ -58,7 +51,20 @@ namespace NewWave.Generator
 				_drums.Notes.Add(grooveNotes);
 			}
 
-			return measures;
+			return _measures;
+		}
+
+		private void GetChordProgression()
+		{
+			do
+			{
+				Chords = ChordProgressionGenerator.ChordProgression(Pitch.G0, MinorOrDiminshedFilter);
+			} while (Chords.Count <= 2 && Chords.Count(c => c.Quality == ChordQuality.Minor) < 2);
+
+			if (Chords.Count == 3)
+			{
+				AugmentThreeChordProgression();
+			}
 		}
 
 		private double GetLength(PercussionNote gn, int i, IReadOnlyList<PercussionNote> kicks)
@@ -69,24 +75,22 @@ namespace NewWave.Generator
 			return length;
 		}
 
-		private static List<Chord> AugmentThreeChordProgression(List<Chord> chords)
+		private void AugmentThreeChordProgression()
 		{
-			if (chords.Count != 3) throw new ArgumentException("Must have three chords");
+			if (Chords.Count != 3) throw new ArgumentException("Must have three chords");
 
 			switch (new[] { 0, 1, 2 }[Randomizer.GetWeightedIndex(new List<double> { 0.2, 0.3, 0.5 })])
 			{
 				case 0: // Extend chord 1
-					chords.Insert(1, chords[0]);
+					Chords.Insert(1, Chords[0]);
 					break;
 				case 1: // Extend chord 2
-					chords.Insert(2, chords[1]);
+					Chords.Insert(2, Chords[1]);
 					break;
 				default: // Extend chord 3
-					chords.Add(chords[2]);
+					Chords.Add(Chords[2]);
 					break;
 			}
-
-			return chords;
 		}
 
 		private static Func<MarkovChainNode<Chord>, MarkovChainNode<Chord>> MinorOrDiminshedFilter
@@ -98,6 +102,11 @@ namespace NewWave.Generator
 						? new MarkovChainNode<Chord>(n.Data, n.Probability * 8.0, n.ChildNodes?.Where(c => c.Probability > 0.08).ToList())
 						: n;
 			}
+		}
+
+		private void GetGroove()
+		{
+			_groove = GrooveLibrary.AllGrooves[Randomizer.Next(GrooveLibrary.AllGrooves.Count)];
 		}
 	}
 }
---
+++ NewWave.Test/GeneratorTests/SectionTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NewWave.Core;
+using NewWave.Generator;
+using NewWave.Midi;
+
+namespace NewWave.Test.GeneratorTests
+{
+	[TestClass]
+	public class SectionTests
+	{
+		[TestMethod]
+		public void SectionTest()
+		{
+			var guitarR = new InstrumentTrack(Instrument.DistortionGuitar, Pan.Right, new List<List<Note>>());
+			var guitarL = new InstrumentTrack(Instrument.DistortionGuitar, Pan.Left, new List<List<Note>>());
+			var bass = new InstrumentTrack(Instrument.ElectricBassFinger, Pan.Center, new List<List<Note>>());
+			var drums = new PercussionTrack(new List<List<PercussionNote>>());
+			var section = new SongSection(TimeSignature.CommonTime, guitarR, guitarL, bass, drums);
+
+			Console.WriteLine(string.Join(" - ", section.Chords));
+		}
+	}
+}
