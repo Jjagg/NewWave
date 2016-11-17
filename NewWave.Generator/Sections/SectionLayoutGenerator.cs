448093bc904f111dd2952cc6ef180ff5f8f89b4b
Andrew Arnold <andrew.s.arnold@gmail.com>
2016-11-16 20:09:04 -0500

    Markov generator for making a song layout

---
+++ NewWave.Generator/Sections/SectionLayoutGenerator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NewWave.Generator.Sections
+{
+	internal static class SectionLayoutGenerator
+	{
+		internal static IEnumerable<SectionType> GetSectionLayout()
+		{
+			return FillChain(n => n).ToList();
+		}
+
+		private static List<MarkovChainNodeFunc<SectionType>> Chain => new List<MarkovChainNodeFunc<SectionType>>
+		{
+			new MarkovChainNodeFunc<SectionType>(SectionType.Intro, 0.7, VerseChain),
+			new MarkovChainNodeFunc<SectionType>(SectionType.Verse, 0.3, VerseChain)
+		};
+
+		private static List<Func<MarkovChainNodeFunc<SectionType>>> VerseChain => new List<Func<MarkovChainNodeFunc<SectionType>>>
+		{
+			() => new MarkovChainNodeFunc<SectionType>(SectionType.Verse, 0.2, VerseChain),
+			() => new MarkovChainNodeFunc<SectionType>(SectionType.Prechorus, 0.4, PrechorusChain),
+			() => new MarkovChainNodeFunc<SectionType>(SectionType.Chorus, 0.4, ChorusChain)
+		};
+
+		private static List<Func<MarkovChainNodeFunc<SectionType>>> PrechorusChain => new List<Func<MarkovChainNodeFunc<SectionType>>>
+		{
+			() => new MarkovChainNodeFunc<SectionType>(SectionType.Chorus, 1, ChorusChain)
+		};
+
+		private static List<Func<MarkovChainNodeFunc<SectionType>>> ChorusChain => new List<Func<MarkovChainNodeFunc<SectionType>>>
+		{
+			() => new MarkovChainNodeFunc<SectionType>(SectionType.Verse, 0.6, VerseChain),
+			() => new MarkovChainNodeFunc<SectionType>(SectionType.Bridge, 0.3, BridgeChain),
+			() => new MarkovChainNodeFunc<SectionType>(SectionType.Outro, 0.1, OutroChain)
+		};
+
+		private static List<Func<MarkovChainNodeFunc<SectionType>>> BridgeChain => new List<Func<MarkovChainNodeFunc<SectionType>>>
+		{
+			() => new MarkovChainNodeFunc<SectionType>(SectionType.Verse, 0.3, VerseChain),
+			() => new MarkovChainNodeFunc<SectionType>(SectionType.Verse, 0.7, ChorusChain)
+		};
+
+		private static List<Func<MarkovChainNodeFunc<SectionType>>> OutroChain => new List<Func<MarkovChainNodeFunc<SectionType>>>
+		{
+			() => new MarkovChainNodeFunc<SectionType>(SectionType.Verse, 1, OutroChain)
+		};
+
+		private static IEnumerable<SectionType> FillChain(Func<MarkovChainNodeFunc<SectionType>, MarkovChainNodeFunc<SectionType>> filterFunc)
+		{
+			var sections = new List<SectionType>();
+			var chain = Chain;
+
+			do
+			{
+				var section = MarkovChainNodeFunc<SectionType>.Choose(chain, filterFunc);
+				if (section != null)
+				{
+					sections.Add(section.Data);
+					chain = section.ChildNodes.Select(n => n()).ToList();
+				}
+			} while (chain != null && chain.Count > 0 && sections.Last() != SectionType.Outro);
+
+			return sections;
+		}
+	}
+}
--- NewWave.Test/GeneratorTests/SectionTests.cs
+++ NewWave.Test/GeneratorTests/SectionTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NewWave.Core;
@@ -22,6 +21,16 @@ namespace NewWave.Test.GeneratorTests
 			{
 				var chordHere = section.Chords.FirstOrDefault(c => c.Item1 == beat);
 				Console.WriteLine("{0} {1} - {2}", beat % section.Time.BeatCount == 0 ? "-" : " ", beat, chordHere?.Item2);
+			}
+		}
+
+		[TestMethod]
+		public void SectionLayoutTest()
+		{
+			var structure = SectionLayoutGenerator.GetSectionLayout();
+			foreach (var sectionType in structure)
+			{
+				Console.WriteLine(sectionType);
 			}
 		}
 	}
