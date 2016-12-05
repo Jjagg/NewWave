7b0a58003004ab6fec45f4930f3a339196569d4c
Andrew S. Arnold <andrew.s.arnold@gmail.com>
2016-12-05 15:02:39 -0500

    Riff generator - Modulate number of "hits"

---
+++ NewWave.Generator/Riffs/RiffGenerator.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NewWave.Core;
+
+namespace NewWave.Generator.Riffs
+{
+	public class RiffGenerator
+	{
+		private readonly TimeSignature _timeSignature;
+		private readonly List<double> _hits;
+
+		public RiffGenerator(TimeSignature timeSignature, IEnumerable<double> hits)
+		{
+			_timeSignature = timeSignature;
+			_hits = hits.ToList();
+		}
+
+		public IEnumerable<double> Rhythm(double resolution, int feel)
+		{
+			var targetBeatCount = resolution * _hits.Count;
+			return resolution < 1.0
+				? ReduceRhythm(targetBeatCount)
+				: IncreaseRhythm(targetBeatCount, feel);
+		}
+
+		private IEnumerable<double> IncreaseRhythm(double targetBeatCount, int feel)
+		{
+			var tryCount = 0;
+			while (_hits.Count < targetBeatCount && tryCount < 20)
+			{
+				tryCount = TryAddHit(tryCount, feel);
+			}
+			return _hits;
+		}
+
+		private int TryAddHit(int tryCount, int feel)
+		{
+			var beatsWithNoHit = Enumerable.Range(0, _timeSignature.BeatCount).Where(b => !_hits.Contains(b)).ToList();
+			if (beatsWithNoHit.Any())
+			{
+				var next = beatsWithNoHit[Randomizer.Next(beatsWithNoHit.Count)];
+				_hits.Add(next);
+			}
+			else if (feel == 4)
+			{
+				var halfBeatsWithNoHit = Enumerable.Range(0, _timeSignature.BeatCount * 2).Where(b => !_hits.Contains(b / 2.0)).ToList();
+				if (halfBeatsWithNoHit.Any())
+				{
+					var next = halfBeatsWithNoHit[Randomizer.Next(halfBeatsWithNoHit.Count)];
+					_hits.Add(next / 2.0);
+				}
+				else
+				{
+					tryCount++;
+				}
+			}
+			return tryCount;
+		}
+
+		private IEnumerable<double> ReduceRhythm(double targetBeatCount)
+		{
+			var tryCount = 0;
+			while (_hits.Count > targetBeatCount && tryCount < 20)
+			{
+				var options = _hits.Where(h => h > 0).ToList();
+				_hits.Remove(options[Randomizer.Next(options.Count)]);
+				tryCount ++;
+			}
+			return _hits;
+		}
+	}
+}
---
+++ NewWave.Test/GeneratorTests/RiffTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NewWave.Core;
+using NewWave.Generator;
+using NewWave.Generator.Grooves;
+using NewWave.Generator.Riffs;
+
+namespace NewWave.Test.GeneratorTests
+{
+	[TestClass]
+	public class RiffTests
+	{
+		[TestMethod]
+		public void RiffResolutionTest()
+		{
+			const int feel = 4;
+			var timeSignature = new TimeSignature(8, 4);
+			var songInfo = new SongInfo(timeSignature, feel);
+			var groove = GrooveGenerator.GenerateGroove(songInfo);
+			WriteRhythm("Original groove", groove.Beats, timeSignature.BeatCount, feel);
+
+			for (var i = 1; i <= 10; i ++)
+			{
+				var res = 4.0 / i;
+				var riffBeats = new RiffGenerator(timeSignature, groove.Beats).Rhythm(res, feel);
+				WriteRhythm(string.Format("{0:0}% resolution", res * 100), riffBeats, timeSignature.BeatCount, feel);
+			}
+		}
+
+		private void WriteRhythm(string label, IEnumerable<double> lengths, int lengthInBeats, int feel)
+		{
+			var spacesPerBeat = feel;
+			var tabLength = lengthInBeats * spacesPerBeat;
+			Console.WriteLine(label);
+			Console.WriteLine(string.Join("", Enumerable.Range(0, lengthInBeats).Select(i => string.Format("{0}{1}", i + 1, new string('.', feel - 1)))));
+			Console.WriteLine(string.Join("", Enumerable.Range(0, tabLength).Select(i => lengths.Any(n => n * spacesPerBeat == i) ? "o" : "-")));
+			Console.WriteLine();
+		}
+	}
+}
