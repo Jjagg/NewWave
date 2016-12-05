41de3ef0c2b5c3c23b698c015e0942ceaaed25f8
Andrew S. Arnold <andrew.s.arnold@gmail.com>
2016-12-05 17:00:57 -0500

    Naive attempt to limit guitar playing to what's physically possible based on the tuning

--- NewWave.Generator/Sections/RiffStrummer.cs
+++ NewWave.Generator/Sections/RiffStrummer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using NewWave.Core;
 using NewWave.Library.Chords;
+using NewWave.Library.Tunings;
 using NewWave.Midi;
 
 namespace NewWave.Generator.Sections
@@ -16,7 +17,7 @@ namespace NewWave.Generator.Sections
 			_riff = riff;
 		}
 
-		public void AddGuitarNotes(InstrumentTrack[] tracks, List<Tuple<int, Chord>> chords, int measure, SongInfo songInfo)
+		public void AddGuitarNotes(IEnumerable<InstrumentTrack> tracks, List<Tuple<int, Chord>> chords, int measure, SongInfo songInfo)
 		{
 			AddNotes(tracks, chords, measure, songInfo);
 		}
@@ -36,7 +37,10 @@ namespace NewWave.Generator.Sections
 				var noteLength = i < _riff.Count - 1
 					? _riff[i + 1] - start
 					: songInfo.TimeSignature.BeatCount - start;
-				var pitches = chords.Last(c => c.Item1 <= measure * songInfo.TimeSignature.BeatCount + start).Item2.Pitches();
+				var chord = chords.Last(c => c.Item1 <= measure * songInfo.TimeSignature.BeatCount + start).Item2;
+				var pitches = isBass
+					? new[] { chord.Pitches()[0] }
+					: PlayableNotes(chord, songInfo.Parameters.GuitarTuning);
 
 				var pitchCount = 100;
 				if (isBass)
@@ -51,6 +55,38 @@ namespace NewWave.Generator.Sections
 			{
 				track.Notes.Add(notes);
 			}
+		}
+
+		private static IEnumerable<Pitch> PlayableNotes(Chord chord, GuitarTuning tuning)
+		{
+			var chordPitches = chord.Pitches();
+			chordPitches.Add(chordPitches[0] + 7);
+			chordPitches.Add(chordPitches[0] + 12);
+			var root = chordPitches[0];
+			var pitches = new List<Pitch> { root };
+
+			// Bottom string is the highest string in the tuning whose open pitch is lower than root
+			var bottomStringIndex = 0;
+			for (var i = tuning.Pitches.Length - 1; i >= 0; i--)
+			{
+				if (tuning.Pitches[i] < root)
+				{
+					bottomStringIndex = i;
+					break;
+				}
+			}
+			
+			var bottomStringFingering = root - tuning.Pitches[bottomStringIndex];
+
+			var openPitchesAboveBottomString = Enumerable.Range(bottomStringIndex + 1, tuning.Pitches.Length - 1 - bottomStringIndex).Select(i => tuning.Pitches[i]).ToList();
+			var pitchesWeCanPlay = openPitchesAboveBottomString.SelectMany(p =>
+			{
+				var pitchesAroundFinger = Enumerable.Range(bottomStringFingering - 2, 5).Where(f => f >= 0).Select(f => p + f);
+				return new[] { p }.Union(pitchesAroundFinger).ToList();
+			}).ToList();
+			pitches.AddRange(pitchesWeCanPlay.Where(p => chordPitches.Contains(p)));
+
+			return pitches;
 		}
 	}
 }
